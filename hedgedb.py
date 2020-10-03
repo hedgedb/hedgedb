07545f3922f1ef71558a4b2d323252eaed0fa21e
Rostyslav Rava <rostyslav@ravasoft.com>
2020-10-03 22:59:23 +0300

    #1: Added display for help message

--- hedgedb.py
+++ hedgedb.py
@@ -1,15 +1,62 @@
+import sys
+
+
 class HedgeDB:
+
+    version = 1.0
+
     def __init__(self):
-        self.task = TaskVersion()
-        self.task.run()
+
+        self.names = []
+        self.commands = dict()
+
+        for command_subclass in Command.__subclasses__():
+            command_class = command_subclass()
+            self.names.append(command_class.name)
+            self.commands[command_class.name] = command_class
+
+        if len(sys.argv) == 2:
+            command_name = sys.argv[1]
+        else:
+            command_name = "help"
+
+        if command_name == "help":
+            self.commands["version"].run(self.commands)
+
+        self.commands[command_name].run(self.commands)
 
 
-class TaskVersion:
+class Command:
     def __init__(self):
-        self.version = 1.0
+        self.name = "name"
+        self.description = "description"
 
-    def run(self):
-        print("HedgeDB V{}".format(self.version))
+    def run(self, commands):
+        pass
 
 
-hedgeDB = HedgeDB()
+class CommandHelp(Command):
+    def __init__(self):
+        super().__init__()
+        self.name = "help"
+        self.description = "Display help message"
+
+    def run(self, commands):
+        print("Usage:")
+        print("  {} command\n".format(sys.argv[0]))
+        print("Commands:")
+        for name, command in commands.items():
+            print("  {:<16} {}".format(name, command.description))
+
+
+class CommandVersion(Command):
+    def __init__(self):
+        super().__init__()
+        self.name = "version"
+        self.description = "Display HedgeDB version"
+
+    def run(self, commands):
+        print("HedgeDB Version {}\n".format(HedgeDB.version))
+
+
+hedge_db = HedgeDB()
